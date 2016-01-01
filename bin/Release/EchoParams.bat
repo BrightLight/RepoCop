0f93705035ef57ea6fc0015f8c301d702aa5bd21
markushastreiter <mh-git@silverseed.com>
2016-01-01 16:46:37 +0100

    Make unittests work in Release build
    Added EchoParams.bat to "release" folder so that AppVeyor can
    successfully run unittest for release build.

---
+++ bin/Release/EchoParams.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+REM the first argument is returned as the exit code
+REM the second argument is returned in standard out
+REM the third argument is returned in error our
+@echo %2
+@echo %3 >>&2
+@exit %1

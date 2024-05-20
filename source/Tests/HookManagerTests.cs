5c871e53362e9b1bc1b183a922ba5fda0972fbfb
BrightLight <ml@silverseed.de>
2024-05-20 10:04:33 +0200

    Add test for nested conditions

--- source/Tests/HookManagerTests.cs
+++ source/Tests/HookManagerTests.cs
@@ -19,10 +19,13 @@
 namespace Silverseed.RepoCop.Tests
 {
   using System;
+  using System.Collections.Generic;
   using System.IO;
   using System.Text;
   using System.Threading.Tasks;
   using NUnit.Framework;
+  using Silverseed.Core.Conditions;
+  using Silverseed.RepoCop.Subversion;
   using VerifyNUnit;
 
   /// <summary>
@@ -96,6 +99,54 @@ namespace Silverseed.RepoCop.Tests
       Assert.That(instructions, Is.Not.Null);
       return Verifier.Verify(instructions.ToString())
         .UseDirectory("VerifiedResults");
+    }
+
+    /// <summary>
+    /// Verify that nested conditions work correctly.
+    /// </summary>
+    /// <remarks>
+    /// This test builds the equivalent of the following XML configuration:
+    /// AndCondition
+    ///   AuthorCondition = AB or CD or EG
+    ///   OrCondition
+    ///     ChangedPathCondition = /special/domainpath1/.*
+    ///     ChangedPathCondition = /special/domainpath2/.*
+    ///     ChangedPathCondition = /special/domainpath3/.*
+    /// </remarks>
+    [TestCase("ab", "/special/domainpath1/readme.txt", "/special/domainpath2/readme.md", ExpectedResult = true, Description = "Allowed author; Both files belong to special domain.")]
+    [TestCase("ab", "/special/domainpath1/readme.txt", "/normal/domainpath2/readme.md", ExpectedResult = true, Description = "Allowed author; One file belongs to special domain.")]
+    [TestCase("ab", "/normal/domainpath1/readme.txt", "/normal/domainpath2/readme.md", ExpectedResult = false, Description = "Allowed author; Both files outside of special domain.")]
+    [TestCase("xy", "/special/domainpath1/readme.txt", "/special/domainpath2/readme.md", ExpectedResult = false, Description = "Disallowed author; Both files belong to special domain.")]
+    [TestCase("xy", "/special/domainpath1/readme.txt", "/normal/domainpath2/readme.md", ExpectedResult = false, Description = "Disallowed author; One file belongs to special domain.")]
+    [TestCase("xy", "/normal/domainpath1/readme.txt", "/normal/domainpath2/readme.md", ExpectedResult = false, Description = "Disallowed author; Both files outside of special domain.")]
+    public bool CheckNestedCondition(string author, string affectedFile1, string affectedFile2)
+    {
+      // arrange
+      var authorCondition = new AuthorCondition(["ab", "cd", "ef"]);
+      var changedPathCondition1 = new ChangedPathCondition("/special/domainpath1/.*");
+      var changedPathCondition2 = new ChangedPathCondition("/special/domainpath2/.*");
+      var changedPathCondition3 = new ChangedPathCondition("/special/domainpath3/.*");
+      var orCondition = new OrCondition(false);
+      orCondition.Add(changedPathCondition1);
+      orCondition.Add(changedPathCondition2);
+      orCondition.Add(changedPathCondition3);
+
+      var andCondition = new AndCondition(false);
+      andCondition.Add(authorCondition);
+      andCondition.Add(orCondition);
+
+      // act
+      List<IRepoAffectedItem> affectedItems =
+      [
+        new SvnLookRepoAffectedItem(RepositoryItemAction.Modify, RepositoryItemNodeKind.File, affectedFile1),
+        new SvnLookRepoAffectedItem(RepositoryItemAction.Modify, RepositoryItemNodeKind.File, affectedFile2),
+      ];
+      List<string> capabilities = ["MergeInfo"];
+      var repoChangeInfo = new SvnLookRepoChangeInfo(HookType.PostCommit, author, "My first commit", 13, new DateTime(2022, 10, 1), affectedItems, capabilities);
+      RepositoryInfoHub.Instance.RepoChangeInfo = repoChangeInfo;
+
+      // assert
+      return andCondition.State;
     }
   }
 }
